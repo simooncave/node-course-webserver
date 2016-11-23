1f2da896abc270840886e5e2b38d32e6536e97f7
simooncave <simooncave@MacBook-Pro-de-Simon.local>
2016-11-24 09:06:51 +1100

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express ();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+  next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome buddy',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+// / bad - send back json with errorMessage
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Sorry something went wrong'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on the port 3000');
+});
